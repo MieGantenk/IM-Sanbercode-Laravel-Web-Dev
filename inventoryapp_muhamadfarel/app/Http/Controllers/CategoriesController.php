6267e4e3390baccd19ade3548b1cc361bdc55fa6
Muhamad Farel <shellgire0199@gmail.com>
2026-02-07 14:00:49 +0700

    Menambahkan tugas CRUD dengan query builder

--- inventoryapp_muhamadfarel/app/Http/Controllers/CategoriesController.php
+++ inventoryapp_muhamadfarel/app/Http/Controllers/CategoriesController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Category;
+use Illuminate\Support\Facades\DB;
 
 class CategoriesController extends Controller
 {
     public function index() {
-        $categories = Category::all();
+        $categories = DB::table('categories')->get();
 
         return view('src.template.categories', compact('categories'));
     }
@@ -18,42 +18,39 @@ class CategoriesController extends Controller
     }
 
     public function store(Request $request) {
-        Category::create ([
+        DB::table('categories')->insert([
             'name' => $request->name,
-            'description' => $request->description
+            'description' => $request->description,
         ]);
 
         return redirect()->route('categories.index');
     }
 
     public function show($id) {
-        $category = Category::findOrFail($id);
+        $category = DB::table('categories')->where('id', $id)->first();
 
         return view('src.template.show', compact('category'));
     }
 
     public function edit($id) {
-        $category = Category::findOrFail($id);
+        $category = DB::table('categories')->where('id', $id)->first();
 
-        return view ('src.template.edit', compact('category'));
+        return view('src.template.edit', compact('category'));
     }
 
-    public function update(Request $request,$id) {
-        $category = Category::findOrFail($id);
-
-        $category->update([
-            'name'=>$request->name,
-            'description'=>$request->description
+    public function update(Request $request, $id) {
+        DB::table('categories')->where('id', $id)->update([
+            'name' => $request->name,
+            'description' => $request->description,
         ]);
 
         return redirect()->route('categories.index');
     }
 
     public function destroy($id) {
-        $category = Category::findOrFail($id);
-
-        $category->delete();
+        DB::table('categories')->where('id', $id)->delete();
 
         return redirect()->route('categories.index');
     }
 }
+
